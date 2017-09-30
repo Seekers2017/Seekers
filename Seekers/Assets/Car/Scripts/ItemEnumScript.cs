cf48fdf27236be33a7cc30fdc7e8bde86349ef71
ryan0432 <ryan0432@gmail.com>
2017-10-01 01:13:10 +1000

    30/09/2017 Ryan Complete ::Scripts::                          SpawnerScript.cs
                         WIP ::Script::
                             SpawnManagerScript.ca

--- Seekers/Assets/Car/Scripts/ItemEnumScript.cs
+++ Seekers/Assets/Car/Scripts/ItemEnumScript.cs
@@ -1,6 +1,6 @@
 ﻿public enum ItemNum
 {
-    Bumper,
     HeathKit,
+    Bumper,
     SpeedBoost
 }
--- Seekers/Assets/Car/Scripts/SpawnManagerScript.cs
+++ Seekers/Assets/Car/Scripts/SpawnManagerScript.cs
@@ -1,14 +1,17 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using System;
+using Random = UnityEngine.Random;
 
 public class SpawnManagerScript : MonoBehaviour
 {
-	// Use this for initialization
-	void Start ()
+
+    // Use this for initialization
+    void Awake()
 	{
-		
-	}
+
+    }
 	
 	// Update is called once per frame
 	void Update ()
