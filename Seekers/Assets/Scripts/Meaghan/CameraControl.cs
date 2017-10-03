f32c72300abaffc7eb846c1176346293c5954a40
ResidentUnleashed <31601762+ResidentUnleashed@users.noreply.github.com>
2017-10-03 16:35:46 +1100

    Changes to the AI, player and camera. These changes include: -Changing to a car model -Adding a drift hop -Various operations to make code clearer

--- Seekers/Assets/Scripts/Meaghan/CameraControl.cs
+++ Seekers/Assets/Scripts/Meaghan/CameraControl.cs
@@ -4,6 +4,8 @@ using UnityEngine;
 
 public class CameraControl : MonoBehaviour {
 
+
+
     //Variables
     public float rotateSpeed = 10.0f;
     public bool debug;
@@ -21,11 +23,25 @@ public class CameraControl : MonoBehaviour {
 	// Update is called once per frame
 	void Update ()
     {
+
+        
+
         if (debug == true)
         {
             //Rotate the camera
             transform.Rotate(Input.GetAxis("RightHorizontal") * Vector3.up * Time.deltaTime * rotateSpeed);
             transform.Rotate(Input.GetAxis("RightVertical") * Vector3.right * Time.deltaTime * rotateSpeed);
+
+            if (Input.GetKey(KeyCode.LeftShift) && Input.GetMouseButtonDown(0))
+            {
+                RaycastHit hit = new RaycastHit();
+                Ray r = Camera.main.ScreenPointToRay(new Vector3(Input.mousePosition.x, Input.mousePosition.y, 10.0f));
+
+                if (Physics.Raycast(r, out hit, 100))
+                {
+                    target = hit.transform;
+                }
+            }
         }
     }
 
@@ -33,6 +49,9 @@ public class CameraControl : MonoBehaviour {
     {
         if (debug == false)
         {
+
+           
+
             //Set the ideal pos is the target's position
             Vector3 targetForward = target.forward;
             targetForward.y = 0.0f;
--- Seekers/Assets/Scripts/Meaghan/Player.cs
+++ Seekers/Assets/Scripts/Meaghan/Player.cs
@@ -5,39 +5,61 @@ using UnityEngine;
 public class Player : MonoBehaviour {
 
     //TODO:
-    //Diagonal movement on drift
-
-
+    //Bumper (collisions)
+    //Items  (collisions and effects)
+    //Drifting (turning bug)
 
 
     //Variables
-    public float speed = 20.0f;
-    public float rotation = 5.0f;
-    public Rigidbody rb;
+    //Public
+    [Header("Alter")]
+    public float speed = 3500.0f;
+    public float rotation = 200.0f; 
+    public float negativeDrift = 0.25f;
+    public float positiveDrift = 2.0f;
+    public float driftSpeed = 0.25f;
+    public float driftRotation = 100.0f;
+    public float driftHopVelocity = 2.0f;
+    public float jumpSpeed = 1000.0f;
+    public float jumpAmount = 0.15f;
+
+    [Header("Do Not Alter")]
     public Transform playerMesh;
-    private float z;
-    private float turn;
+
+    //Private
     private bool canStoreTurn;
     private float prevTurn;
     private bool isDriftingRight;
     private bool isDriftingLeft;
     private Quaternion targetRotation;
     private bool canRotate;
+    private Rigidbody rb;
+    private float turn;
+    private bool canJump;
+    private float storePrevYpos;
+    private bool canStoreYPos;
+    private bool grounded;
+    private bool hasJumped;
+    private bool falling;
+    private bool canMove;
 
-	// Use this for initialization
-	void Start ()
+
+    // Use this for initialization
+    void Start ()
     {
         rb = GetComponent<Rigidbody>();
         canStoreTurn = true;
         canRotate = true;
+        canJump = true;
+        canStoreYPos = true;
+        grounded = true;
+        canMove = true;
+        falling = false;
     }
 	
 	// Update is called once per frame
 	void FixedUpdate ()
-    {
-        //Update z float
-        z = Input.GetAxis("Vertical");
-        
+    {   
         //Get the horizontal axis
         turn = Input.GetAxis("Horizontal");
 
@@ -47,11 +69,11 @@ public class Player : MonoBehaviour {
             //Restrict turn based on what direction you're travelling
             if (turn < 0.0f)
             {
-                turn *= 0.25f;
+                turn *= negativeDrift;
             }
             else if (turn > 0.0f)
             {
-                turn *= 2.0f;
+                turn *= positiveDrift;
             }
         }
         else if (isDriftingLeft == true)
@@ -59,11 +81,11 @@ public class Player : MonoBehaviour {
             //Restrict turn based on what direction you're travelling
             if (turn < 0.0f)
             {
-                turn *= 2.0f;
+                turn *= positiveDrift;
             }
             else if (turn > 0.0f)
             {
-                turn *= 0.25f;
+                turn *= negativeDrift;
             }
         }
 
@@ -76,8 +98,16 @@ public class Player : MonoBehaviour {
         //If the "A" button is pressed
         if (Input.GetButton("Fire1"))
         {
-            //Move forward
-            rb.AddForce(transform.forward * speed * Time.fixedDeltaTime);
+            if(canMove == true)
+            {
+                //Move forward
+                rb.AddForce(transform.forward * speed * Time.fixedDeltaTime);
+            }
+            else
+            {
+                rb.AddForce(Vector3.zero);
+            }
+            
         }
         //If the "B" button is pressed
         else if (Input.GetButton("Fire2"))
@@ -90,53 +120,116 @@ public class Player : MonoBehaviour {
         //If the right trigger is pressed
         if (Input.GetAxis("Right Trigger") == 1)
         {
-            //If we can store the turn
-            if(canStoreTurn == true)
+            if(canJump == true)
             {
-                //store the turn
-                prevTurn = turn;
-                //say we can't store the turn
-                canStoreTurn = false;
+                //Don't push the player forward
+                canMove = false;
+
+                //If we can store the Y pos
+                if (canStoreYPos == true)
+                {
+                    //Store the y pos
+                    storePrevYpos = transform.position.y;
+
+                    //Stored so break out
+                    canStoreYPos = false;
+                }
+
+                if (falling == false)
+                {
+                    //Jump
+                    rb.AddForce(transform.up * speed * Time.fixedDeltaTime);
+
+                    if (transform.position.y > storePrevYpos + jumpAmount)
+                    {
+                        falling = true;
+                    }
+                }
+
+                if (falling == true)
+                {
+                    //Fall
+                    rb.AddForce((transform.up * -1.0f) * -speed * Time.fixedDeltaTime);
+                    hasJumped = true;
+
+                    //If we have hit the ground
+                    if (grounded == true)
+                    {
+                        canJump = false;
+                    }
+                }
             }
 
-            if (canStoreTurn == false)
+            //If we can't jump
+            if(canJump == false)
             {
-                if (canRotate == true)
-                {
-                    //Drift
-                    targetRotation = transform.rotation * Quaternion.Euler(new Vector3(0.0f, 100.0f * prevTurn, 0.0f));
-                    canRotate = false;
-                }
-               
-                //If right turn
-                if (prevTurn > 0.0f)
-                {
-                    //Add a diagonal force and restrict the forward velocity
-                    rb.AddForce((transform.forward + transform.right).normalized * speed * 0.25f * Time.fixedDeltaTime);
+                canMove = true;
 
-                    //It's drifting
-                    isDriftingRight = true;
-                }
-                //Else if left turn
-                if(prevTurn < 0.0f)
+                //If we can store the turn
+                if (canStoreTurn == true)
                 {
-                    //Add a diagonal force and restrict the forward velocity
-                    rb.AddForce((transform.forward + transform.right).normalized * speed * 0.25f * Time.fixedDeltaTime);
-
-                    //It's drifting
-                    isDriftingLeft = true;
+                    //store the turn
+                    prevTurn = turn;
+                    //say we can't store the turn
+                    canStoreTurn = false;
                 }
-               
+
+                if (canStoreTurn == false)
+                {
+                    if (canRotate == true)
+                    {
+                        //Makes the 
+                        if(prevTurn > 0.0f && prevTurn < 1.0f)
+                        {
+                            prevTurn = 1.0f;
+                        }
+                        else if (prevTurn < 0.0f && prevTurn > -1.0f)
+                        {
+                            prevTurn = -1.0f;
+                        }
+
+                        //Drift
+                        targetRotation = transform.rotation * Quaternion.Euler(new Vector3(0.0f, driftRotation * prevTurn, 0.0f));
+                        canRotate = false;
+                    }
+
+                    //If right turn
+                    if (prevTurn > 0.0f)
+                    {
+                        //Add a diagonal force and restrict the forward velocity
+                        rb.AddForce((transform.forward + transform.right).normalized * speed * driftSpeed * Time.fixedDeltaTime);
+
+                        //It's drifting
+                        isDriftingRight = true;
+                    }
+                    //Else if left turn
+                    if (prevTurn < 0.0f)
+                    {
+                        //Add a diagonal force and restrict the forward velocity
+                        rb.AddForce((transform.forward + transform.right).normalized * speed * driftSpeed * Time.fixedDeltaTime);
+
+                        //It's drifting
+                        isDriftingLeft = true;
+                    }
+
+                }
             }
+           
         }
-        else
+
+        
+        if(Input.GetAxis("Right Trigger") != 1)
         {
+            //Setting variables if the tigger is not pressed back to non drifitng state
             playerMesh.transform.localRotation = Quaternion.identity;
             canStoreTurn = true;
             isDriftingRight = false;
             isDriftingLeft = false;
             canRotate = true;
-            targetRotation = Quaternion.Euler(new Vector3(0, turn, 0));
+            canStoreYPos = true;
+            canJump = true;
+            hasJumped = false;
+            falling = false;
         }
 
 
@@ -146,4 +239,21 @@ public class Player : MonoBehaviour {
             transform.rotation = new Quaternion(0, transform.rotation.y, transform.rotation.z, transform.rotation.w);
         }
     }
+
+
+    void OnCollisionEnter(Collision a_other)
+    {
+        //If you have jumped, check the collision
+        if(hasJumped == true)
+        {
+            //If it is the track edge
+            if (a_other.collider.gameObject.CompareTag("Ground"))
+            {
+                grounded = true;
+            }
+        } 
+    }
 }
+
+
+
