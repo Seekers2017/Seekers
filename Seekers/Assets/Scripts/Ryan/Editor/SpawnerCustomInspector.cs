e17231570cd178f71337e88ff05fd961a71dece8
ResidentUnleashed <31601762+ResidentUnleashed@users.noreply.github.com>
2017-10-09 16:27:52 +1100

    More stuff

---
+++ Seekers/Assets/Scripts/Ryan/Editor/SpawnerCustomInspector.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+
+//[CustomEditor(typeof(SpawnManagerScript))]
+//[CanEditMultipleObjects]
+
+public class SpawnerCustomInspector : Editor
+{
+    //SpawnerScript spawnerScript;
+    //SpawnManagerScript spawnManagerScript;
+    //GameObject spawnManager;
+
+    public override void OnInspectorGUI()
+    {
+        //spawnerScript = GameObject.Find("SpawnerScript").GetComponent<SpawnerScript>();
+        //spawnManagerScript = GameObject.Find("SpawnManagerScript").GetComponent<SpawnManagerScript>();
+        //spawnManager = GameObject.Find("SpawnManager").GetComponent<GameObject>();
+
+        //displays original GUI
+        //base.OnInspectorGUI();
+
+        //get access to the attached script (SpawnManager itself)
+        //SpawnManagerScript myTarget = (SpawnManagerScript)target;
+
+        //EditorGUILayout.MinMaxSlider(myTarget.hthKitSpawnLmt, myTarget.transform.childCount);
+
+        //EditorGUILayout.MinMaxSlider(0, 10);
+
+
+        //this.Repaint();
+
+        //EditorGUILayout.IntField(myTarget.transform.childCount);
+
+
+    }
+}
