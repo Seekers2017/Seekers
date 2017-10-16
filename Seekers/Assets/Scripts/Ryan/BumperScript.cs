8e2197af2bbe5f10bdee21a00e2a0b74e5357c2a
ryan0432 <ryan0432@gmail.com>
2017-10-16 14:38:15 +1100

    Bumper Script and UI

--- Seekers/Assets/Scripts/Ryan/BumperScript.cs
+++ Seekers/Assets/Scripts/Ryan/BumperScript.cs
@@ -1,19 +1,49 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
-using System;
 using UnityEngine;
 
-public class Bumper : Item
+public class BumperScript : MonoBehaviour
 {
+    public bool isFrontBumper;
+    private bool isAlive;
+
+    public float lifeSpan;
+    
 	// Use this for initialization
 	void Start ()
-	{
-
+    {
+        isAlive = true;
     }
 	
 	// Update is called once per frame
 	void Update ()
-	{
-		
-	}
+    {
+        CheckBumperLiveSpan();
+    }
+
+    //Set up what will happen after collision
+    private void OnTriggerEnter(Collider other)
+    {
+        //if the collision target is a Bumper and it's alive
+        if (other.CompareTag("Bumper") && isFrontBumper == false)
+        {
+            //set isAlive to false
+            other.gameObject.GetComponent<BumperScript>().isAlive = false;
+            //Destroy the bumper
+            Destroy(other.transform.gameObject);
+        }
+    }
+
+    private void CheckBumperLiveSpan()
+    {
+        if (lifeSpan <= 0 && isAlive == true && isFrontBumper == false)
+        {
+            isAlive = false;
+            Destroy(this);
+        }
+        else
+        {
+            lifeSpan -= Time.deltaTime;
+        }
+    }
 }
---
+++ Seekers/Assets/Scripts/Ryan/IngameUIScript.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class IngameUIScript : MonoBehaviour
+{
+    private int currLap;
+
+    private Sprite lapCountSprite;
+    private Sprite rankSprite;
+    private Sprite[] lapSpriteList;
+    private Sprite[] rankSpriteList;
+
+	// Use this for initialization
+	void Start ()
+    {
+        currLap = GameObject.FindGameObjectWithTag("Player").GetComponent<CarCheckpointScript>().currLap;
+
+        lapCountSprite = gameObject.transform.Find("lapCount").GetComponent<Sprite>();
+        rankSprite = gameObject.transform.Find("rank").GetComponent<Sprite>();
+
+        lapSpriteList = Resources.LoadAll<Sprite>("LapSprites");
+        rankSpriteList = Resources.LoadAll<Sprite>("RankSprites");
+    }
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        lapCountSprite = lapSpriteList[currLap - 1];
+    }
+
+    //private CheckItem
+}
