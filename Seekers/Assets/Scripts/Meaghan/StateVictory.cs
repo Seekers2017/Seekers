faef42d231ed1f4cca1e8c7c187e0a9c0d463fdd
ResidentUnleashed <31601762+ResidentUnleashed@users.noreply.github.com>
2017-11-28 15:34:42 +1100

    Win/Lose state

--- Seekers/Assets/Scripts/Meaghan/StateVictory.cs
+++ Seekers/Assets/Scripts/Meaghan/StateVictory.cs
@@ -9,6 +9,7 @@ public class StateVictory : MonoBehaviour {
     //Variables
     private GameStateManagerScript gameManager;
     private PlayerManager playerManager;
+    private WheelDrive wheelDrive;
 
     [SerializeField]
     private Sprite winSprite;
@@ -27,24 +28,30 @@ public class StateVictory : MonoBehaviour {
         //Obtain the script properties
         gameManager = GameObject.Find("GameManager").GetComponent<GameStateManagerScript>();
         playerManager = GameObject.Find("Player").GetComponent<PlayerManager>();
+        wheelDrive = GameObject.Find("Player").GetComponent<WheelDrive>();
         set = gameObject.transform.GetChild(0).GetComponent<Image>();
     }
 	
 	// Update is called once per frame
 	void Update ()
     {
-		//Switch to the main menu
 
         //If you have won
         if(playerManager.Win == true)
         {
             //Set the winning sprite
             set.sprite = winSprite;
+
+            //Stop driving
+            wheelDrive.AbilityToDrive = false;
         }
         else
         {
             //Set the losing sprite
             set.sprite = loseSprite;
+
+            //Stop driving
+            wheelDrive.AbilityToDrive = false;
         }
 
         if (XCI.GetButtonDown(XboxButton.A))
--- Seekers/Assets/Scripts/Meaghan/StateVictoryP2.cs
+++ Seekers/Assets/Scripts/Meaghan/StateVictoryP2.cs
@@ -9,6 +9,7 @@ public class StateVictoryP2 : MonoBehaviour {
     //Variables
     private GameStateManagerScript gameManager;
     private PlayerManager playerManager;
+    private WheelDrive wheelDrive;
 
     [SerializeField]
     private Sprite winSprite;
@@ -27,24 +28,30 @@ public class StateVictoryP2 : MonoBehaviour {
         //Obtain the script properties
         gameManager = GameObject.Find("GameManager").GetComponent<GameStateManagerScript>();
         playerManager = GameObject.FindGameObjectWithTag("Player2").GetComponent<PlayerManager>();
+        wheelDrive = GameObject.FindGameObjectWithTag("Player2").GetComponent<WheelDrive>();
         set = gameObject.transform.GetChild(0).GetComponent<Image>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        //Switch to the main menu
 
         //If you have won
         if (playerManager.Win == true)
         {
             //Set the winning sprite
             set.sprite = winSprite;
+
+            //Stop driving
+            wheelDrive.AbilityToDrive = false;
         }
         else
         {
             //Set the losing sprite
             set.sprite = loseSprite;
+
+            //Stop driving
+            wheelDrive.AbilityToDrive = false;
         }
 
         if (XCI.GetButtonDown(XboxButton.A))
